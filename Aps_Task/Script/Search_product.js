9591556e745510b51482136a61f44f36cf570f30
Ogbuifymark <ogbuifymark@gmail.com>
2023-03-15 22:39:30 +0100

    Finished implementation

---
+++ Aps_Task/Script/Search_product.js
@@ -0,0 +1,19 @@
+﻿function Search_product()
+{
+  //Opens the specified URL in a running instance of the specified browser.
+  Browsers.Item(btChrome).Navigate("https://lava.mt/");
+  //Sets the specified position and size for the specific BrowserWindow object.
+  Aliases.browser.BrowserWindow.Maximize();
+  //Clicks the 'searchboxSearchInput1' control.
+  Aliases.browser.pageLavaMtMaltaSLargestOnlineMar.header.searchboxSearchInput1.Click();
+  //Sets the text 'microwave' in the 'searchboxSearchInput1' text editor.
+  Aliases.browser.pageLavaMtMaltaSLargestOnlineMar.header.searchboxSearchInput1.SetText("microwave");
+  //Clicks the 'vg' control.
+  Aliases.browser.pageLavaMtMaltaSLargestOnlineMar.header.vg.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageLavaMtMaltaSLargestOnlineMar.Wait();
+  //Clicks the 'image06' control.
+  Aliases.browser.pageLavaMtMaltaSLargestOnlineMar.textnode2.textnode3.linkProduct2.image06.Click();
+  //Checks whether the 'contentText' property of the Aliases.browser.pageLavaMtProductOfferBomannMicr.textnode object equals 'Bomann Microwave Mw6014Cb 700W Whi'.
+  aqObject.CheckProperty(Aliases.browser.pageLavaMtProductOfferBomannMicr.textnode, "contentText", cmpEqual, "Bomann Microwave Mw6014Cb 700W Whi");
+}
